b774727ae63281d5cd203571b45762bd81008cd0
cdiamond3 <diamond.chris.3@gmail.com>
2021-04-11 15:47:57 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+main.remove();
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = "victory";
+newHeader.innerHTML = "Chris is the champion";
